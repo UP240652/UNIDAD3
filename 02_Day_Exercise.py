36959aa5afedc8c04e20a7d4b7fc64ca70cccf71
UP240652 <up240652@alumnos.upa.edu.mx>
2025-03-18 00:28:42 -0600

    Add files via upload

---
+++ 02_Day_Exercise.py
@@ -0,0 +1,59 @@
+#Day 2: 30 Days of python programming
+import math
+
+first_name = 'Fernanda'
+last_name = 'Rodriguez'
+country = 'USA'
+city = 'Snohomish'
+age = 20
+is_married =True
+skills = ['HTML', 'CSS', 'JS', 'React', 'Python']
+person_info = {
+   'firstname':'Fernanda',
+   'lastname':'Rodriguez',
+   'country':'USA',
+   'city':'Snohomish'
+   }
+
+print(type(first_name))
+print(type(last_name))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(is_married))
+print(type(skills))
+print(type(person_info))
+
+first_name_len = len(first_name)
+last_name_len = len(last_name)
+
+num_one = 5
+num_two = 4
+
+total = num_one + num_two
+diff = num_two - num_one
+prod = num_two * num_one
+div = num_one / num_two
+remainder = num_two % num_one
+exp = num_one ** num_two
+floor_division = num_one // num_two
+
+radius = 30
+area = math.pi*radius*radius
+
+circulo_circuferencia = 2*math.pi*radius
+
+radius = int(input())
+area = math.pi*radius*radius
+
+
+first_name = input()
+last_name = input()
+country = input()
+age =int(input())
+
+
+help('keywords') 
+
+
+
